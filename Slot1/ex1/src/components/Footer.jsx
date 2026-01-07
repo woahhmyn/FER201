d1ec16b36a87226bd94681833790a2ba0de2fa17
woahhmyn <hmyndooo@gmail.com>
2026-01-08 00:18:24 +0700

    Thêm mới Footer.jsx

---
+++ Slot1/ex1/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import avatar from "../avatar.png";
+
+function Footer() {
+    return(
+        <>
+        <img
+                src={avatar}
+                alt="avatar"
+                style={{
+                    width: "70px",
+                    height: "70px",
+                    borderRadius: "50%",
+                    objectFit: "cover"
+                }}
+            />
+        <h1> Trang web tạo bởi &copy; hmyndooo </h1>
+        <p> Liên hệ: <a href="mailto:hmyndooo@gmail.com">hmyndooo@gmail.com</a> </p>
+        </>
+    );
+}
+
+export default Footer;
